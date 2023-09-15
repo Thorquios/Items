0fd7e60cacb7ff08df74279df4b0e73ad77ebbaf
Thorquios <36553632+Thorquios@users.noreply.github.com>
2023-09-15 18:30:15 +0200

    Add files via upload

---
+++ ProyectoItemsEmpresa/src/main/java/Grupo4/ProyectoItemsEmpresa/HackABossProyectoItemsEmpresaApplication.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoItemsEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackABossProyectoItemsEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackABossProyectoItemsEmpresaApplication.class, args);
+	}
+
+}
---
+++ ProyectoItemsEmpresa/src/test/java/Grupo4/ProyectoItemsEmpresa/HackABossProyectoItemsEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoItemsEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackABossProyectoItemsEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
